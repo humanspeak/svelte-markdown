b07f89b75a06a7c009937bfc05d62386cba9ce14
Jason Kummerl <jason.h@kummerl.com>
2024-11-04 14:05:05 -0500

    Fix sentry loading

--- docs/src/hooks.server.ts
+++ docs/src/hooks.server.ts
@@ -1,20 +1,15 @@
 import { env } from '$env/dynamic/public'
-import * as Sentry from '@sentry/sveltekit'
-import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit'
+import * as Sentry from '@sentry/cloudflare'
+import type { Handle } from '@sveltejs/kit'
 import { sequence } from '@sveltejs/kit/hooks'
 
-Sentry.init({
-    dsn: env.PUBLIC_SENTRY_DSN,
-    environment: env.PUBLIC_ENVIRONMENT ?? 'local',
-
-    tracesSampleRate: 1.0
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: import.meta.env.DEV,
-})
+export const sentryHandle: Handle = async ({ event, resolve }) => {
+    Sentry.sentryPagesPlugin(() => ({
+        environment: env.PUBLIC_ENVIRONMENT ?? 'local',
+        dsn: env.PUBLIC_SENTRY_DSN
+    }))
+    return await resolve(event)
+}
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle())
-
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry()
+export const handle = sequence(sentryHandle)
