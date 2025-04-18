9f7695fc7784e10c36751c5999097d5b13abb612
Jason Kummerl <jason.h@kummerl.com>
2025-04-18 12:02:57 -0400

    Bring out HtmlRenderers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,12 @@
 // Reexport your entry components here
+import type { HtmlRenderers } from '$lib/renderers/html/index.js'
 import type { RendererComponent, Renderers, Token, TokensList } from '$lib/utils/markdown-parser.js'
 import SvelteMarkdown from './SvelteMarkdown.svelte'
 import type { SvelteMarkdownOptions, SvelteMarkdownProps } from './types.js'
 
 export default SvelteMarkdown
 export type {
+    HtmlRenderers,
     RendererComponent,
     Renderers,
     SvelteMarkdownOptions,
