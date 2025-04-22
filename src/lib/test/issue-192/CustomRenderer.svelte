af026628a3f3330d54ceac6777fd0335bd9495b0
Jason Kummerl <jason.h@kummerl.com>
2025-04-22 10:35:11 -0400

    Update the readme with issue 192's learnings

---
+++ src/lib/test/issue-192/CustomRenderer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte'
+
+    interface Props {
+        children?: Snippet
+        href?: string
+        title?: string
+        text?: string
+    }
+
+    const { href = '', title = '', text = '', children }: Props = $props()
+</script>
+
+<a {href} {title} class="custom-link">
+    {#if children}
+        {@render children?.()}
+    {:else}
+        {text}
+    {/if}
+</a>
---
+++ src/routes/test/issue-192/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import SvelteMarkdown, { type Renderers } from '$lib/index.js'
+    import CustomRenderer from '$lib/test/issue-192/CustomRenderer.svelte'
+
+    const source = `
+# This is a header
+
+This is a paragraph with **bold** and <em>mixed HTML</em>.
+
+* List item with \`inline code\`
+* And a [link](https://svelte.dev)
+  * With nested items
+  * Supporting full markdown
+`
+
+    const renderers: Partial<Renderers> = {
+        link: CustomRenderer
+    }
+</script>
+
+<SvelteMarkdown {source} {renderers} />
