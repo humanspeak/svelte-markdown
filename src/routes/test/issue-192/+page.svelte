1bfe3b0cd5362e6615f1740e9393280a5d9493d4
Jason Kummerl <jason.h@kummerl.com>
2025-05-17 04:30:29 -0400

    Update test case for more visibility

--- src/routes/test/issue-192/+page.svelte
+++ src/routes/test/issue-192/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import SvelteMarkdown, { type Renderers } from '$lib/index.js'
+    import SvelteMarkdown, { type Renderers, type Token, type TokensList } from '$lib/index.js'
     import CustomRenderer from '$lib/test/issue-192/CustomRenderer.svelte'
 
     let source = `
@@ -12,8 +12,12 @@ This is a paragraph with **bold** and <em>mixed HTML</em>.
   * With nested items
   * Supporting full markdown
 
-[![MPF](https://avatars.githubusercontent.com/u/162604590?u=548f358e716a223731ab372776a09723cf815f4d&v=4&size=80)](https://avatars.githubusercontent.com/u/162604590?u=548f358e716a223731ab372776a09723cf815f4d&v=4&size=80)
+  [![image](https://avatars.githubusercontent.com/u/162604590?s=64&u=548f358e716a223731ab372776a09723cf815f4d&v=4)](https://stackblitz.com/edit)
 `
+
+    const parsed = async (parsedTokens: Token[] | TokensList) => {
+        console.log('displaying tokens', parsedTokens)
+    }
 
     const renderers: Partial<Renderers> = {
         link: CustomRenderer
@@ -24,6 +28,28 @@ This is a paragraph with **bold** and <em>mixed HTML</em>.
     <textarea bind:value={source} placeholder="Enter markdown here" data-testid="markdown-input">
     </textarea>
     <div class="preview">
-        <SvelteMarkdown {source} {renderers} />
+        <SvelteMarkdown {source} {renderers} {parsed} />
     </div>
 </div>
+
+<style>
+    .container {
+        display: flex;
+        gap: 1rem;
+        width: 100%;
+    }
+
+    textarea {
+        width: 50%;
+        min-height: 300px;
+        padding: 1rem;
+        font-family: monospace;
+    }
+
+    .preview {
+        width: 50%;
+        padding: 1rem;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+</style>
