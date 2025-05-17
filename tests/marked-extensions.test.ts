c5add6819f8f8739aa05b26b9e61fb44e4a566e5
Jason Kummerl <jason.h@kummerl.com>
2025-05-17 04:48:33 -0400

    Renaming for consistency

--- tests/marked-extensions.test.ts
+++ tests/marked-extensions.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test.describe('Alert Tokenizer Extension', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('/test/marked_extensions')
+        await page.goto('/test/marked-extensions')
         await page.waitForSelector('[data-testid="markdown-input"]')
     })
 
