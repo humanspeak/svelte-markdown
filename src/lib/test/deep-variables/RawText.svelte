36d19f0b8aa0a63b2f1493f161fb5a4859f75125
Jason Kummerl <jason.h@kummerl.com>
2025-07-12 15:26:59 -0400

    Current stop

---
+++ src/lib/test/deep-variables/RawText.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    interface Props {
+        text?: string
+        someDeepVariable?: Record<string, string>
+    }
+    const { text, someDeepVariable }: Props = $props()
+
+    $inspect('someDeepVariable', someDeepVariable)
+</script>
+
+<div>
+    <p>{text} {someDeepVariable?.text ?? 'no text'}</p>
+</div>
---
+++ src/routes/test/deep-variables/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import SvelteMarkdown from '$lib/SvelteMarkdown.svelte'
+    import RawText from '$lib/test/deep-variables/RawText.svelte'
+
+    let source = '#&nbsp;Hello You are amazing!!! :D '
+    const renderers = {
+        rawtext: RawText
+    }
+</script>
+
+<div class="container">
+    <textarea bind:value={source} placeholder="Enter markdown here" data-testid="markdown-input">
+    </textarea>
+    <div class="preview">
+        <SvelteMarkdown {source} {renderers} someDeepVariable={{ text: 'Hello Deep Variable' }} />
+    </div>
+</div>
+
+<style>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 1rem;
+        padding: 1rem;
+    }
+
+    textarea {
+        min-height: 200px;
+        padding: 0.5rem;
+    }
+
+    .preview {
+        padding: 0.5rem;
+    }
+</style>
