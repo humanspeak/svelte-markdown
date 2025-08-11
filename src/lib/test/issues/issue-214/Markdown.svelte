cb00575d768e913f132c7610d48f867f29bb24cc
Jason Kummerl <jason.h@kummerl.com>
2025-08-11 16:52:36 -0400

    Commend update

--- src/lib/test/issues/issue-214/Markdown.svelte
+++ src/lib/test/issues/issue-214/Markdown.svelte
@@ -10,7 +10,6 @@
         // Allow only the minimal markdown set needed for inline formatting
         // paragraph/text wrappers are required for inline tokens to render
         ...allowRenderersOnly(['paragraph', 'text', 'strong', 'em', 'link', 'codespan']),
-        // Allow only strong, em, link(a), code (HTML tags)
         html: buildUnsupportedHTML()
     }}
 />
